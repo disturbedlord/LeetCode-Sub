88ea4f7cbfdfee851b535cef1b100b3001f6382e
Avinash K <44549238+disturbedlord@users.noreply.github.com>
2024-08-08 20:23:55 +0530

    Time: 3 ms (37.70%) | Memory: 8.4 MB (39.22%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+
+    vector<pair<int,int>> directions = {{0 , 1} , {1 , 0} , {-1 , 0}, {0 , -1}};
+    inline static int next = 0;
+    pair<int,int> nextStep(){
+        int nn = next;
+        next ++;
+        next %= 4;
+        return directions[nn];
+    }
+
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
+        next = 0;
+        int n = mat.size();
+        int m = mat[0].size();
+        if(n == 0 && m == 0)return {};
+        vector<vector<int>> seen = vector<vector<int>>(n , vector<int>(m , 0));
+        vector<int> res = vector<int>();
+        //res[0] = mat[0][0];
+        //seen[0][0] = 1;
+        int idx = 0;
+        int x = 0 , y = 0;
+        auto nStep = nextStep();
+        x = nStep.first, y = nStep.second;
+        int posX = 0 , posY = 0;
+        pair<int,int> prev = {0,0};
+        while(idx < (n * m)){
+            if((posX >= n || posX < 0 || posY >= m || posY < 0) || seen[posX][posY]){
+                nStep = nextStep();
+                x = nStep.first;
+                y = nStep.second;
+                posX = prev.first;posY = prev.second;
+                posX += x ; posY += y;
+                continue;
+            }
+            //cout<<posX<<" : "<<posY<<endl;
+            res.push_back(mat[posX][posY]);
+            seen[posX][posY] = 1;
+            prev = {posX , posY};
+            posX += x;
+            posY += y;
+            idx++;
+        }
+
+        return res;
+    }
+};
