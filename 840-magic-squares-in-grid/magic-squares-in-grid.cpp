9dd36cedfd908b1390234199145e50d28b16349e
Avinash K <44549238+disturbedlord@users.noreply.github.com>
2024-08-13 07:28:52 +0530

    Time: 6 ms (22.53%) | Memory: 11.1 MB (48.21%) - LeetSync

---
+++ 840-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -0,0 +1,99 @@
+class Solution {
+public:
+    vector<vector<int>> cloneGrid(int n, int m) {
+        return vector<vector<int>>(n, vector<int>(m, 0));
+    }
+
+    void print(vector<vector<int>>& grid) {
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                cout << grid[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    bool validateUnique(int i,int j , vector<vector<int>> g){
+        vector<int> seen = vector<int>(9 , 0);
+        for(int a=i;a<i+3;a++){
+            for(int b=j;b<j+3;b++){
+                int k = g[a][b];
+                if(k >= 10)return 0;
+                if(k == 0)return 0;
+                if(seen[k-1])return 0;
+                seen[k - 1] = 1;
+
+            }
+        }
+
+        return 1;
+    }
+
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int n = grid.size();    // no of rows
+        int m = grid[0].size(); // no of cols
+        auto row = cloneGrid(n, m);
+        auto col = cloneGrid(n, m);
+        int res = 0;
+
+        if (n < 3 || m < 3)
+            return 0;
+
+        // find RowSum
+        for (int i = 0; i < n; i++)
+            row[i][0] = grid[i][0]; // populate First Row
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                row[i][j] = row[i][j - 1] + grid[i][j];
+                if (j >= 3) {
+                    row[i][j] -= grid[i][j - 3];
+                }
+            }
+        }
+
+         //print(row);
+
+        // find ColSum
+        for (int i = 0; i < m; i++)
+            col[0][i] = grid[0][i]; // populate first Column
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                col[i][j] = col[i - 1][j] + grid[i][j];
+                if (i >= 3) {
+                    col[i][j] -= grid[i - 3][j];
+                }
+            }
+        }
+         //print(col);
+
+        for (int i = 0; i <= n - 3; i++) {
+            for (int j = 0; j <= m - 3; j++) {
+                 //cout<<i<<":"<<j<<endl;
+                int diag1 =
+                    grid[i][j] + grid[i + 1][j + 1] + grid[i + 2][j + 2];
+                 //cout<<diag1<<" ";
+                int diag2 = grid[i][j + 2] + grid[i + 1][j + 2 - 1] +
+                            grid[i + 2][j + 2 - 2];
+                 //cout<<diag2<<" ";
+                int rowS =
+                    row[i][j + 2] + row[i + 1][j + 2] + row[i + 2][j + 2];
+                int colS =
+                    col[i + 2][j] + col[i + 2][j + 1] + col[i + 2][j + 2];
+
+                                         //cout<<rowS<<" "<<colS<<" ";
+                                         //cout<<row[i][j + 2]<<"|"<<row[i+1][j + 2]<<"|"<<row[i+2][j+2]<<" ";
+                                         //cout<<col[i+2][j]<<"|"<<col[i+2][j+1]<<"|"<<col[i+2][j+2]<<" ";
+                bool X = row[i][j + 2] == row[i + 1][j + 2] && row[i+1][j+2] == row[i + 2][j + 2];
+                bool Y = col[i + 2][j] == col[i + 2][j + 1] && col[i+2][j+1] == col[i + 2][j + 2];
+                //cout<<X<<" L "<<Y<<" ";
+                if (X && Y && validateUnique(i,j,grid)) {
+                    if (rowS == colS && rowS + colS == 90 && diag1 == diag2 &&
+                        diag1 + diag2 == 30)
+                        res++;
+                }
+            }
+        }
+
+        return res;
+    }
+};
