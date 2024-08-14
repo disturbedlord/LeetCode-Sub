56645e51e520e63bf17ef558ff61c25275459aae
Avinash K <44549238+disturbedlord@users.noreply.github.com>
2024-08-14 13:36:43 +0530

    Time: 3 ms (77.80%) | Memory: 14.8 MB (18.43%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -30,7 +30,7 @@ public:
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         int n = candidates.size();
-        sort(candidates.begin() , candidates.end());
+        sort(candidates.begin() , candidates.end()); //O(nLogn)
         int total = accumulate(candidates.begin() , candidates.end() , 0);
         if(total < target) return {};
         knapSack(candidates , n , 0 , target , {});
