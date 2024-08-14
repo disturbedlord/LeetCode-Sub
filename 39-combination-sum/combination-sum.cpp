97db3aa72cd1cf6406cde9e6d674b67edddf53a3
Avinash K <44549238+disturbedlord@users.noreply.github.com>
2024-08-14 12:01:31 +0530

    Time: 9 ms (38.06%) | Memory: 15 MB (41.46%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<vector<int>>res;
-    set<vector<int>> seen;
     void solve(vector<int>& c , int t , int i , vector<int> arr , int& n){
         
         if(i >= n || t < 0) return;
 
-        if(t == 0 && seen.count(arr) == 0){
+        if(t == 0){
             res.push_back(arr);
-            seen.insert(arr);
         }
 
-        arr.push_back(c[i]);
-        solve(c , t - c[i] , i , arr , n);
-        arr.pop_back();
-        solve(c , t , i + 1 , arr , n);
+        for(;i<n;i++){
+            if(t - c[i] < 0) return;
+            arr.push_back(c[i]);
+            solve(c , t - c[i] , i , arr , n);
+            arr.pop_back();
+        }
+        // solve(c , t , i + 1 , arr , n);
     }
 
     vector<vector<int>> combinationSum(vector<int>& c, int target) {
