485ebde62b01ee969916c6fe7e162ae59d3bd592
Avinash K <44549238+disturbedlord@users.noreply.github.com>
2024-08-29 19:07:13 +0530

    Time: 15 ms (69.58%) | Memory: 17.9 MB (32.50%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool res = false;
+    vector<vector<int>> adj;
+    int END;
+    vector<int> seen;
+    bool findLoop(int idx){
+        if(seen[idx] == 2) return false;
+        if(seen[idx] == 1){return true;}
+        seen[idx] = 1;
+        for(auto next : adj[idx]){
+            if(adj[next].size() != 0 && findLoop(next))
+                return true;
+        }
+        seen[idx] = 2;
+        return false;
+    }
+
+    void createAdjacencyMatrix(int n , vector<vector<int>>& mat){
+        adj = vector<vector<int>>(n);
+        for(auto m : mat){
+            adj[m[1]].push_back(m[0]);
+        }
+        int i = 0;
+        // for(auto x : adj){
+        //     cout<<i++<<" -> ";
+        //     for(auto y : x){
+        //         cout<<y<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        adj.clear();
+        END = numCourses - 1;
+        createAdjacencyMatrix(numCourses , prerequisites);
+        seen = vector<int>(END + 1 , 0);
+        for(int i=0;i<numCourses;i++){
+            // for(auto ele : seen){
+            //     cout<<ele<<" ";
+                
+            // }
+            // cout<<endl;
+            if(seen[i] != 2 && adj[i].size() != 0 && findLoop(i)){cout<<i<<endl;return false;}
+            // seen[i] = 2;
+        }
+        return true;
+    }
+};
