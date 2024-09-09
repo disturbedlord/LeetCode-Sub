a560fde8a335bd89590a40e8add619712f120f5a
Avinash K <44549238+disturbedlord@users.noreply.github.com>
2024-09-09 22:24:09 +0530

    Time: 39 ms (39.57%) | Memory: 35.4 MB (55.60%) - LeetSync

--- 1306-jump-game-iii/jump-game-iii.cpp
+++ 1306-jump-game-iii/jump-game-iii.cpp
@@ -14,9 +14,21 @@ public:
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size();
         vector<int> visited = vector<int>(n , 0);
-        bool isReachable = false;
-        isReachable = check(arr , start , n , visited);
+        queue<int> positions;
+        positions.push(start);
+        //DFS APPROACH
+        //return check(arr , start , n , visited);
 
-        return isReachable;
+        while(!positions.empty()){
+            auto front = positions.front();
+            positions.pop();
+            if(front < 0 || front >= n || visited[front]) continue;
+            if(arr[front] == 0) return true;
+            visited[front] = 1;
+            positions.push(front - arr[front]);
+            positions.push(front + arr[front]);
+        }
+
+        return false;
     }
 };
