f94e526416321496b629f0fde457bd93e3483dd6
Avinash K <44549238+disturbedlord@users.noreply.github.com>
2024-09-22 10:39:54 +0530

    Time: 262 ms (5.04%) | Memory: 50 MB (9.26%) - LeetSync

---
+++ 743-network-delay-time/network-delay-time.cpp
@@ -0,0 +1,70 @@
+using vec = vector<vector<int>>;
+class Node{
+    public:
+        int u;
+        int v;
+        int weight;
+        Node(int c , int a , int b){
+            v = a;
+            u = c;
+            weight = b;
+        }
+};
+using vec2 = vector<vector<Node>>;
+class Solution {
+public:
+    
+
+    vec2 createAdjacencyList(vec& times , int n){
+        vector<vector<Node>> adjList = vector<vector<Node>>(n + 1 , vector<Node>());
+        //cout<<adjList[0].size();
+        for(auto time : times){
+            int u = time[0];
+            int v = time[1];
+            int w = time[2];
+            cout<<u<<"-"<<v<<"-"<<w<<endl;
+            adjList[u].push_back(Node(u , v , w));
+        }
+        
+        for(auto a : adjList){
+            for(auto b : a){
+                cout<<b.u<<" "<<b.v<<" "<<b.weight<<endl;
+            }
+        }
+
+        return adjList;
+    }
+
+    int networkDelayTime(vector<vector<int>>& times, int n, int start) {
+        vec2 adjList = createAdjacencyList(times , n);
+
+        vector<int> dist = vector<int>(n + 1 , INT_MAX);
+
+        priority_queue<pair<int,int>> queue;
+        queue.push({0 , start});
+        dist[start] = 0;
+        int res = 0;
+        while(!queue.empty()){
+            auto u = queue.top();
+            queue.pop();
+            int weight = u.first;
+            for(auto v : adjList[u.second]){
+                if(v.weight + weight < dist[v.v]){
+                    dist[v.v] = v.weight + weight;
+                    queue.push({dist[v.v] , v.v});
+                }
+            }
+        }
+
+        for(int l = 1;l<=n;l++){
+            //cout<<dist[l]<<endl;
+            if(dist[l] == INT_MAX) {return -1;}
+            
+            else {
+                res = max(res , dist[l]);
+            }
+        }
+
+        return res;
+    }
+};
